3f9c4fd3056d7602bb5b36984d5c4ae89eb6b77e
Markus Lampe <idnkhbtfu@gmail.com>
2024-02-20 18:45:39 +0100

    better service worker for offline capability

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,36 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
+const CACHE_NAME = 'v1'
 
-self.addEventListener('activate', (event) => {
-  self.clients.claim();
-});
+self.addEventListener('install', async event => {
+  const cache = await caches.open(CACHE_NAME)
+  await cache.addAll(['app.js', 'icon.png', 'index.html', 'manifest.json']);
+})
 
-self.addEventListener('fetch', (event) => {
-});
+self.addEventListener('activate', async event => {
+  const cacheNames = await caches.keys()
+  for (let cacheName in cacheNames) {
+    if (cacheName !== CACHE_NAME) {
+      await caches.delete(cacheName);
+    }
+  }
+
+  await self.clients.claim();
+})
+
+self.addEventListener('fetch', async event => {
+  const cachedResponse = await caches.match(event.request)
+  const freshResponse = await fetch(event.request)
+
+  if (freshResponse.ok) {
+    const cache = caches.open(CACHE_NAME)
+    await cache.put(event.request, freshResponse.clone())
+    await event.respondWith(freshResponse)
+    return;
+  }
+
+  if (cachedResponse) {
+    await event.respondWith(cachedResponse)
+    return;
+  }
+
+  await event.respondWith(freshResponse)
+})
